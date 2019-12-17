33418fdc55f7ec620347c9f180bf5ff0826cf5ea
kosyachniy <polozhev@mail.ru>
2019-12-17 23:07:15 +0300

    NGINX config

--- web/src/sets.js
+++ web/src/sets.js
@@ -1,16 +1,16 @@
 export const server = {
-	link: 'http://127.0.0.1:5000/',
+	link: 'http://127.0.0.1:5600/',
 	ip: '127.0.0.1',
-	port: 5000,
+	port: 5600,
 }
 export const client = {
-	link: 'http://127.0.0.1:3000',
+	link: 'http://127.0.0.1:3600',
 	ip: '127.0.0.1',
-	port: 3000,
+	port: 3600,
 }
 
 export const socket = {
-    link: 'http://127.0.0.1:5000/',
+    link: 'http://127.0.0.1:5600/',
 };
 
 export const name = 'Telegram'
